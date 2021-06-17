7a8fb1875b84a73c8b3730a1d7a37c95a3adf396
chiragmahajan146 <chiragmahajan146@gmail.com>
2021-06-17 23:35:46 +0530

    created private and public pages and used context api

---
+++ src/components/PublicRoute.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
+
+const PrivateRoute = ({ children, ...routeProps }) => {
+  const { profile, isLoading } = useProfile();
+
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" context="Laoding" speed="Slow" />
+      </Container>
+    );
+  }
+  if (profile && !isLoading) {
+    return <Redirect to="/" />;
+  }
+  return <Route {...routeProps}>{children}</Route>;
+};
+
+export default PrivateRoute;
---
+++ src/misc/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyCyfc0wwF1_h15FOqZA2ZF_jpeeESUbEf0',
+  authDomain: 'chat-web-app-425f7.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-425f7-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-425f7',
+  storageBucket: 'chat-web-app-425f7.appspot.com',
+  messagingSenderId: '931835060990',
+  appId: '1:931835060990:web:07fdabe9389135be963baf',
+};
+
+const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
